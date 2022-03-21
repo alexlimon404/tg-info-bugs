818c3368bd3180b48dee1bf74324a186ad90e440
alexlimon404 <mirrorbitmirror@gmail.com>
2022-03-21 12:26:12 +0300

    add TG_INFO_BUGS_ENABLE

--- config/tg-info-bugs.php
+++ config/tg-info-bugs.php
@@ -1,6 +1,7 @@
 <?php
 
 return [
+    'enable' => env('TG_INFO_BUGS_ENABLE', false),
     'api_token' => env('TG_INFO_BUGS_TOKEN'),
     'api_url' => env('TG_INFO_BUGS_URL', 'https://tg-info.forumbase.net/api/'),
     'channels' => env('TG_INFO_BUGS_CHANNELS')
